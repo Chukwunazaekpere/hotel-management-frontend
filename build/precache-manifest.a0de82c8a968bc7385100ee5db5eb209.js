a12ba255bc4b0815bd25ca1b14442173ccf9fb32
Chukwunazaekpere <emmannuelchinaza5777@gmail.com>
2020-11-16 16:06:48 +0100

    new build

---
+++ build/precache-manifest.a0de82c8a968bc7385100ee5db5eb209.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0380a4e75a83627b5e4a9bab219ed86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a41a75d3f6d28b6df96",
+    "url": "/static/css/2.994f7b2c.chunk.css"
+  },
+  {
+    "revision": "4f9719c17f72d71d7d29",
+    "url": "/static/css/main.bcba54f5.chunk.css"
+  },
+  {
+    "revision": "0a41a75d3f6d28b6df96",
+    "url": "/static/js/2.e14c398f.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/static/js/2.e14c398f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f9719c17f72d71d7d29",
+    "url": "/static/js/main.a5b430a2.chunk.js"
+  },
+  {
+    "revision": "17987b4882d1533c1950",
+    "url": "/static/js/runtime-main.2a21443b.js"
+  },
+  {
+    "revision": "b921383f07b26326947492c16fe70093",
+    "url": "/static/media/hotel 2.b921383f.jpg"
+  },
+  {
+    "revision": "17b62f180883671a6595b2651e37c877",
+    "url": "/static/media/hotel 3.17b62f18.jpg"
+  }
+]);
